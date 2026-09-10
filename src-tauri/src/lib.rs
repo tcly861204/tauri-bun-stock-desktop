5d9dccf0b60ee0778e7056e5d678d558c94e1e3c
tcly861204 <tcly861204@hotmail.com>
2026-09-10 19:28:14 +0800

    feat: ✨ 完善代码

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,18 +1,20 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-// #[tauri::command]
-// fn greet(name: &str) -> String {
-//     format!("Hello, {}! You've been greeted from Rust!", name)
-// }
-
 // mod corn;
 mod tray;
+use tauri::Manager; 
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        // ⚠️ 单实例插件必须第一个注册
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            if let Some(window) = app.get_webview_window("main") {
+                let _ = window.unminimize();
+                let _ = window.show();
+                let _ = window.set_focus();
+            }
+        }))
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_tauribun::init())
-        // .invoke_handler(tauri::generate_handler![greet])
         .setup(|app| {
             tray::init(app)?;
             // corn::init();
